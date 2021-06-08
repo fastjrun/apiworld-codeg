7b443aec00e3b9da1945aec8d8f8ee078692a0cb
fastjrun <fastjrun@139.com>
2021-06-08 21:54:09 +0800

    新增自定义接口代码生成器示例模块

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "build ..."
+if [ "install_all" = $1 ] ; then
+    mvn clean compile -U -pl apiworld-bundle -am -Dbdgc.skip=false
+    # mvn clean compile
+    mvn clean install -Dbdgc.skip=true
+elif [ "deploy_all" = $1 ] ; then
+    mvn clean compile -U -pl apiworld-bundle -am -Dbdgc.skip=false
+    mvn -U clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests -Dbdgc.skip=true
+elif [ "package_mock_server" = $1 ] ; then
+    mvn clean compile -pl apiworld-bundle-mock -am -Dbdmgc.skip=false
+    mvn clean package -pl apiworld-mock-server -am
+elif [ "clean_all" = $1 ] ; then
+    mvn clean
+    rm -rf apiworld-admin-bundle/src
+    rm -rf apiworld-admin-bundle-mock/src
+fi
+echo "build done."
