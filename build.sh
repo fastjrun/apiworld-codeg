1dcb9b2cee60bcff5d32433983ebed1520aa8e36
fastjrun <fastjrun@139.com>
2021-06-16 21:55:38 +0800

    增加项目使用说明readme

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ elif [ "deploy_all" = $1 ] ; then
     mvn clean compile -U -pl apiworld-bundle -am -Dbdgc.skip=false
     mvn -U clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests -Dbdgc.skip=true
 elif [ "package_mock_server" = $1 ] ; then
-    mvn clean compile -pl apiworld-bundle-mock -am -Dbdmgc.skip=false
-    mvn clean package -pl apiworld-mock-server -am
+    # mvn clean compile -pl apiworld-bundle-mock -am -Dbdmgc.skip=false
+    mvn clean package -pl apiworld-mock-server -am -Dbdmgc.skip=false
 elif [ "clean_all" = $1 ] ; then
     mvn clean
-    rm -rf apiworld-admin-bundle/src
-    rm -rf apiworld-admin-bundle-mock/src
+    rm -rf apiworld-bundle/src
+    rm -rf apiworld-bundle-mock/src
 fi
 echo "build done."
