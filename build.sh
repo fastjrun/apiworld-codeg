54daa65f257c69451116ab828fc1816d320be2c3
崔一凡 <javafun@yeah.net>
2021-09-10 11:22:42 +0800

    升级为2.2.4.RELEASE

--- build.sh
+++ build.sh
@@ -2,15 +2,11 @@
 
 echo "build ..."
 if [ "install_all" = $1 ] ; then
-    mvn clean compile -U -pl apiworld-bundle -am -Dbdgc.skip=false
-    # mvn clean compile
-    mvn clean install -Dbdgc.skip=true
+    mvn clean install -U
 elif [ "deploy_all" = $1 ] ; then
-    mvn clean compile -U -pl apiworld-bundle -am -Dbdgc.skip=false
-    mvn -U clean install org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -DskipTests -Dbdgc.skip=true
+    mvn clean deploy -U
 elif [ "package_mock_server" = $1 ] ; then
-    mvn clean compile -pl apiworld-bundle-mock -am -Dbdmgc.skip=false
-    mvn clean package -pl apiworld-mock-server -am
+    mvn clean package -U -pl apiworld-mock-server -am
 elif [ "clean_all" = $1 ] ; then
     mvn clean
     rm -rf apiworld-bundle/src
