a3f2fb36a9018f3f7b6fe27935bca3fd21f93613
fastjrun <fastjrun@139.com>
2022-02-08 18:14:35 +0800

    调整ProviderMockStartApplication位置

--- apiworld-mock-server/src/main/java/com/fastjrun/mock/ProviderMockStartApplication.java
+++ apiworld-mock-server/src/main/java/com/fastjrun/mock/ProviderMockStartApplication.java
@@ -1,4 +1,4 @@
-package com.fastjrun.apiworld.info;
+package com.fastjrun.mock;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,24 +6,29 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+@Configuration
 @SpringBootApplication(
-        exclude = {DataSourceAutoConfiguration.class, ValidationAutoConfiguration.class}, scanBasePackages = {"com.fastjrun.mock"})
+        exclude = {DataSourceAutoConfiguration.class, ValidationAutoConfiguration.class},
+        scanBasePackages = {"com.fastjrun.mock"}
+)
+@EnableSwagger2
 public class ProviderMockStartApplication {
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(ProviderMockStartApplication.class)
                 .bannerMode(Banner.Mode.CONSOLE)
                 .run(args);
         System.out.println(ProviderMockStartApplication.class.getResource("/"));
+
     }
 
     @Bean
@@ -32,6 +37,6 @@ public class ProviderMockStartApplication {
     }
 
     private ApiInfo apiInfo() {
-        return (new ApiInfoBuilder()).title("快嘉").description("对外开放平台").version("1.0").build();
+        return (new ApiInfoBuilder()).title("快嘉").description("示例模板系统").license("MIT").licenseUrl("http://opensource.org/licenses/MIT").contact(new Contact("fastjrun", "http://github.com/fastjrun", "fastjrun@139.com")).version("1.0").build();
     }
 }
